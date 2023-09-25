f603b7298cd1a429efee51f8d2c59dbac17b59be
Gaspard Rouault <gaspard.rouault@epfedu.fr>
2023-09-25 15:28:29 +0200

    Création des 3 exercices

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gaspard Rouault
+ Rouault, TDB, Afficher Message, Septempbre 2023
+*/public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Bonjour");
+       System.out.println("Aurevoir");
+       String prenom;
+          Scanner sc;
+          sc = new Scanner(System.in);
+          System.out.println("Bonjour, quel est votre prenom et ton nom de famille ?");
+          prenom = sc.nextLine();
+
+        // TODO code application logic here
+    }
+    
+}
