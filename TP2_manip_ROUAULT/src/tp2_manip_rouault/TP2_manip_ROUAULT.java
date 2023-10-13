4d4fa508c71a2048030fd14af0b2d901ec26f560
Gaspard Rouault <gaspard.rouault@epfedu.fr>
2023-10-13 16:42:33 +0200

    TP2-Manip

--- TP2_manip_ROUAULT/src/tp2_manip_rouault/TP2_manip_ROUAULT.java
+++ TP2_manip_ROUAULT/src/tp2_manip_rouault/TP2_manip_ROUAULT.java
@@ -19,7 +19,23 @@ package tp2_manip_rouault;
         Tartiflette assiette3 = assiette2 ;
         //2 Tartiflettes ont été crées, assiette 2 et 3 référencent la meme tartiflette
         System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;System.out.println("nb de calories de Assiette 3 : " +assiette3.nbCalories) ;
-
+        assiette2.nbCalories+=1;
+        System.out.println("nb de calories de Assiette 3 : " +assiette3.nbCalories);
+        
+        Tartiflette inter=new Tartiflette(assiette2.nbCalories);
+        assiette2=inter;
+        assiette2 =assiette1;
+        assiette1=inter;
+        System.out.println("nb de calories de Assiette 1 : " +assiette1.nbCalories) ;System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
+        
+        //Moussaka assiette666 = assiette1 ;  
+        //Moussaka assiette667 = new Tartiflette() ;   Ces deux lignes sont incorrectes
+        // Non une référence d'objet ne peux référencer que le type d'objet qu'elle annonce
+        Moussaka [] tabMoussaka = new Moussaka[10];
+        for (int i=0; i < 10; i++) {
+          tabMoussaka[i]=new Moussaka(i);
+          System.out.println(tabMoussaka[i].nbCalories);
+        }
     }
     
 }
