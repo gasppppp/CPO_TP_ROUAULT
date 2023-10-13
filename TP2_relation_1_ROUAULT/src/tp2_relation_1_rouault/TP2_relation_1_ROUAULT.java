79c5830a05c53e4b430efd7c1aabb8e5a92619cb
Gaspard Rouault <gaspard.rouault@epfedu.fr>
2023-10-13 17:17:45 +0200

    TP2-Relation

---
+++ TP2_relation_1_ROUAULT/src/tp2_relation_1_rouault/TP2_relation_1_ROUAULT.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_relation_1_rouault;
+
+/**
+ *
+ * @author Gaspard Rouault
+ */
+public class TP2_relation_1_ROUAULT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
+        Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+       
+        Personne bob = new Personne("Bobby", "Sixkiller");
+        Personne reno = new Personne("Reno", "Raines");
+        System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        
+        bob.liste_voitures[0] = uneClio ;
+        bob.nbVoitures = 1 ;
+        uneClio.Proprietaire = bob ;
+        
+        bob.liste_voitures[1] = uneAutreClio ;
+        bob.nbVoitures = 2 ;
+        uneAutreClio.Proprietaire = bob ;
+        
+        reno.liste_voitures[0] = une2008 ;
+        reno.nbVoitures = 1 ;
+        une2008.Proprietaire = reno ;
+        
+        reno.liste_voitures[1] = uneMicra ;
+        reno.nbVoitures = 2 ;
+        uneMicra.Proprietaire = reno ;
+        
+        System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
+        System.out.println("la deuxieme voiture de Bob est " +bob.liste_voitures[1] ) ;
+        System.out.println("la premiere voiture de Reno est " +reno.liste_voitures[0] ) ;
+        System.out.println("la premiere voiture de Reno est " +reno.liste_voitures[1] ) ;
+
+    }
+    
+}
