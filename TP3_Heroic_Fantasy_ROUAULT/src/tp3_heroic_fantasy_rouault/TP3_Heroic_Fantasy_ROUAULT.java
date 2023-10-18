0a291fe106ccc8a1f07b423d78bdf15979a526de
Gaspard Rouault <gaspard.rouault@epfedu.fr>
2023-10-18 17:17:24 +0200

    TP3-Heroic_Fantasy

---
+++ TP3_Heroic_Fantasy_ROUAULT/src/tp3_heroic_fantasy_rouault/TP3_Heroic_Fantasy_ROUAULT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_rouault;
+
+/**
+ *
+ * @author Gaspard Rouault
+ *Gaspard Rouault, TDB, Heroic Fantasy, Octobre 2023
+*/public class TP3_Heroic_Fantasy_ROUAULT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Epee epee1=new Epee(5,"");
+        
+    }
+    
+}
