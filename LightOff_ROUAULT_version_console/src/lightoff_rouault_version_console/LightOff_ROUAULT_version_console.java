538a33e6070a3dc8364d67713638b475c6c882d1
Gaspard Rouault <gaspard.rouault@epfedu.fr>
2023-10-27 09:34:27 +0200

    Projet LightOff-Classe GrilledeJeu terminée et fonctionelle

--- LightOff_ROUAULT_version_console/src/lightoff_rouault_version_console/LightOff_ROUAULT_version_console.java
+++ LightOff_ROUAULT_version_console/src/lightoff_rouault_version_console/LightOff_ROUAULT_version_console.java
@@ -19,6 +19,26 @@ public class LightOff_ROUAULT_version_console {
         System.out.println(cellule1.getEtat());
         cellule1.activerCellule();
         System.out.println(cellule1.getEtat());
+        
+        GrilledeJeu grille1=new GrilledeJeu(3,3);
+        
+        
+        
+        grille1.activerLigneColonneOuDiagonaleAleatoire();
+        System.out.println(grille1.toString());
+        grille1.activerDiagonaleMontante();
+        System.out.println(grille1.toString());
+        grille1.activerColonneDeCellules(2);
+        System.out.println(grille1.toString());
+        grille1.activerDiagonaleDescendante();
+        System.out.println(grille1.toString());
+        grille1.activerLigneDeCellules(1);
+        System.out.println(grille1.toString());
+        grille1.cellulesToutesEteintes();
+        System.out.println(grille1.toString());
+        grille1.eteindreToutesLesCellules();
+        System.out.println(grille1.toString());
     }
+    
     
 }
